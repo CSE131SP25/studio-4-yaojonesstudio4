38503b64af97372a1799a8c3c01e3773e2be7425
cindy <cindy@CindyYaoLaptop.dhcp.wustl.edu>
2025-02-19 15:17:39 -0600

    done yay

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,33 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(220, 0, 0);
+		StdDraw.filledRectangle(.5, 0.5, 0.4, 0.25);
+		
+		StdDraw.setPenColor(255, 255, 255);
+		StdDraw.filledRectangle(.5, 0.5, 0.4, 0.05);
+		
+		StdDraw.setPenColor(220, 0, 0);
+		StdDraw.filledRectangle(.5, 0.5, 0.4, 0.04);
+		
+		StdDraw.setPenColor(255, 255, 0);
+		StdDraw.filledCircle(.5, .50, 0.2);
+		
+		StdDraw.setPenColor(220, 0, 0);
+		StdDraw.filledCircle(.5, .50, 0.195);
+		
+		StdDraw.setPenColor(255, 255, 0);
+		StdDraw.filledCircle(.5, .50, 0.175);
+		
+		StdDraw.setPenColor(220, 0, 0);
+		StdDraw.filledCircle(.5, .50, 0.17);
+		
+		StdDraw.setPenColor(255, 255, 0);
+		StdDraw.filledCircle(.5, .5, .15);
+		
+		
+
+		
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,24 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shapeType = in.next();
+		int col1 = in.nextInt();
+		int col2 = in.nextInt();
+		int col3 = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double param1 = in.nextDouble();
+		double param2 = in.nextDouble();
+		double param3 = in.nextDouble();
+		double param4 = in.nextDouble();
+		
+		StdDraw.setPenColor( col1, col2, col3);
+		if (isFilled && (shapeType.equals("rectangle"))){
+			
+		StdDraw.filledRectangle(param1, param2, param3, param4);
+		}
+		else if (!isFilled && (shapeType.equals("rectangle"))) {
+			StdDraw.rectangle(param1, param2, param3, param4);
+		}
 		
 	}
 }
